c8dc39d4f7c2fda1d9d230cb55f01de8ecae557e
ShreyesPD <shreyespd12@gmail.com>
2025-11-19 01:23:34 +0530

    removed build test log

--- front-end/server/api/debug-runtime.get.ts
+++
@@ -1,4 +0,0 @@
-export default defineEventHandler(() => {
-  // Only returns public runtime config (safe). Remove this endpoint after debugging.
-  return { publicRuntime: useRuntimeConfig().public ?? {} }
-})
